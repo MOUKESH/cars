1c5728fe42ef6fa10ef2c6292406c57ff9afe9d0
MOUKESH <moukekasofo@gmail.com>
2026-09-21 02:42:48 +0100

    Update greeting message in main function
    Modified the greeting message in main function.

--- fichier1.cpp
+++ fichier1.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 int main(){
     int r;
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;// Modification du haut par la personne A
     std::cout << "Ton papa" << std::endl;
     std::cout << "Entrer un nombre: ";//l'utilisateur doit entrer un nombre
     int nombre;
