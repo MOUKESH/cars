d6d0f8bd11acfa6fb0c2a5796f184bfb2b680e3e
MOUKESH <moukekasofo@gmail.com>
2026-09-21 02:56:18 +0100

    Add modifications and additional output messages

--- fichier1.cpp
+++ fichier1.cpp
@@ -2,6 +2,8 @@
 int main(){
     int r;
     std::cout << "Hello, World!" << std::endl;// Modification du haut par la personne A
+    std::cout << "Modification du haut par la personne c" << std::endl;//affiche le résultat
+    std::cout << "Modification du bas par la personne D" << std::endl;//affiche le résultat
     std::cout << "Ton papa" << std::endl;
     std::cout << "Entrer un nombre: ";//l'utilisateur doit entrer un nombre
     int nombre;
