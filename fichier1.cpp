f1c42f35d399b91078d7be65997a5d4e02e56600
Moukesh <moukekasofo@gmail.com>
2026-09-21 10:53:22 +0100

    Modification en bas du fichier

--- fichier1.cpp
+++ fichier1.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 int main(){
+    int a = 5;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Modification distante faite sur GitHub" << std::endl;
+    std::cout << "La valeur de a est : " << a << std::endl;
     return 0;
 }
